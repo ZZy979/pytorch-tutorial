ceec285350aed60de94e0d64cb21f29016c152c4
ZZy979 <979481894@qq.com>
2020-09-29 10:20:59 +0800

    使用nn模块实现网络和损失函数

--- two_layer_net.py
+++ two_layer_net.py
@@ -1,56 +1,53 @@
 import torch
-
-dtype = torch.float
-device = torch.device("cpu")
-# device = torch.device("cuda:0")  # Uncomment this to run on GPU
+import torch.nn as nn
 
 # N is batch size; D_in is input dimension;
 # H is hidden dimension; D_out is output dimension.
 N, D_in, H, D_out = 64, 1000, 100, 10
 
-# Create random Tensors to hold input and outputs.
-# Setting requires_grad=False indicates that we do not need to compute gradients
-# with respect to these Tensors during the backward pass.
-x = torch.randn(N, D_in, device=device, dtype=dtype)
-y = torch.randn(N, D_out, device=device, dtype=dtype)
+# Create random Tensors to hold inputs and outputs
+x = torch.randn(N, D_in)
+y = torch.randn(N, D_out)
 
-# Create random Tensors for weights.
-# Setting requires_grad=True indicates that we want to compute gradients
-# with respect to these Tensors during the backward pass.
-w1 = torch.randn(D_in, H, device=device, dtype=dtype, requires_grad=True)
-w2 = torch.randn(H, D_out, device=device, dtype=dtype, requires_grad=True)
+# Use the nn package to define our model as a sequence of layers.
+# nn.Sequential is a Module which contains other Modules, and applies them in sequence to
+# produce its output. Each Linear Module computes output from input using a
+# linear function, and holds internal Tensors for its weight and bias.
+model = nn.Sequential(
+    torch.nn.Linear(D_in, H),
+    torch.nn.ReLU(),
+    torch.nn.Linear(H, D_out),
+)
 
-learning_rate = 1e-6
+# The nn package also contains definitions of popular loss functions;
+# in this case we will use Mean Squared Error (MSE) as our loss function.
+loss_fn = nn.MSELoss(reduction='sum')
+
+learning_rate = 1e-4
 for t in range(500):
-    # Forward pass: compute predicted y using operations on Tensors;
-    # these are exactly the same operations we used to compute the forward pass using Tensors,
-    # but we do not need to keep references to intermediate values since
-    # we are not implementing the backward pass by hand.
-    y_pred = x.mm(w1).clamp(min=0).mm(w2)
+    # Forward pass: compute predicted y by passing x to the model.
+    # Module objects override the __call__ operator so you can call them like functions.
+    # When doing so you pass a Tensor of input data to the Module and it produces
+    # a Tensor of output data.
+    y_pred = model(x)
 
-    # Compute and print loss using operations on Tensors.
-    # Now loss is a Tensor of shape (1,)
-    # loss.item() gets the scalar value held in the loss.
-    loss = (y_pred - y).pow(2).sum()
+    # Compute and print loss. We pass Tensors containing the predicted and true values of y,
+    # and the loss function returns a Tensor containing the loss.
+    loss = loss_fn(y_pred, y)
     if t % 100 == 99:
         print(t, loss.item())
 
-    # Use autograd to compute the backward pass. This call will compute the
-    # gradient of loss with respect to all Tensors with requires_grad=True.
-    # After this call w1.grad and w2.grad will be Tensors holding the gradient
-    # of the loss with respect to w1 and w2 respectively.
+    # Zero the gradients before running the backward pass.
+    model.zero_grad()
+
+    # Backward pass: compute gradient of the loss with respect to all the learnable
+    # parameters of the model. Internally, the parameters of each Module are stored
+    # in Tensors with requires_grad=True, so this call will compute gradients for
+    # all learnable parameters in the model.
     loss.backward()
 
-    # Manually update weights using gradient descent. Wrap in torch.no_grad()
-    # because weights have requires_grad=True, but we don't need to track this in autograd.
-    # An alternative way is to operate on weight.data and weight.grad.data.
-    # Recall that tensor.data gives a tensor that shares the storage with tensor,
-    # but doesn't track history.
-    # You can also use torch.optim.SGD to achieve this.
+    # Update the weights using gradient descent.
+    # Each parameter is a Tensor, so we can access its gradients like we did before.
     with torch.no_grad():
-        w1 -= learning_rate * w1.grad
-        w2 -= learning_rate * w2.grad
-
-        # Manually zero the gradients after updating weights
-        w1.grad.zero_()
-        w2.grad.zero_()
+        for param in model.parameters():
+            param -= learning_rate * param.grad
