7f7d799dddbd63a4ba3c73ee0fed3f67ef3ceafa
ZZy979 <979481894@qq.com>
2020-10-06 17:30:06 +0800

    增加用于异构图连接预测的GNN示例

---
+++ gnn/link_pred_hetero.py
@@ -0,0 +1,61 @@
+"""在异构图上训练用于连接预测任务的GNN
+
+参考：<https://docs.dgl.ai/guide/training-link.html>
+"""
+import dgl
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+from gnn.edge_clf_hetero import HeteroDotProductPredictor
+from gnn.link_pred import compute_loss
+from gnn.node_clf_hetero import RGCN, build_user_item_graph
+
+
+def construct_negative_graph(graph, k, etype):
+    utype, _, vtype = etype
+    src, dst = graph.edges(etype=etype)
+    neg_src = src.repeat_interleave(k)
+    neg_dst = torch.randint(0, graph.number_of_nodes(vtype), (len(src) * k,))
+    return dgl.heterograph(
+        {etype: (neg_src, neg_dst)},
+        num_nodes_dict={ntype: graph.number_of_nodes(ntype) for ntype in graph.ntypes}
+    )
+
+
+class Model(nn.Module):
+
+    def __init__(self, in_features, hidden_features, out_features, rel_names):
+        super().__init__()
+        self.sage = RGCN(in_features, hidden_features, out_features, rel_names)
+        self.pred = HeteroDotProductPredictor()
+
+    def forward(self, g, neg_g, x, etype):
+        h = self.sage(g, x)
+        return self.pred(g, h, etype), self.pred(neg_g, h, etype)
+
+
+def main():
+    hetero_graph = build_user_item_graph()
+    user_feats = hetero_graph.nodes['user'].data['feature']
+    item_feats = hetero_graph.nodes['item'].data['feature']
+    node_features = {'user': user_feats, 'item': item_feats}
+    k = 5
+
+    model = Model(10, 20, 5, hetero_graph.etypes)
+    opt = optim.Adam(model.parameters())
+
+    for epoch in range(10):
+        negative_graph = construct_negative_graph(hetero_graph, k, ('user', 'click', 'item'))
+        pos_score, neg_score = model(
+            hetero_graph, negative_graph, node_features, ('user', 'click', 'item')
+        )
+        loss = compute_loss(pos_score, neg_score)
+        opt.zero_grad()
+        loss.backward()
+        opt.step()
+        print(loss.item())
+
+
+if __name__ == '__main__':
+    main()
